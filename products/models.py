e8de945f3a005ae820f53e02b21f8d299dd4b046
master <dcopm999@gmail.com>
2022-06-29 16:47:44 +0500

    Adding SlugField
    * requirements.txt: Added python-slugify to requirements and updated other pachage versions
    * products/models.py: Added SlugField to Catalog and Product models
    * products/admin.py: Added mptt fileds for sorting
    * .pre-commit-config.yamlL: Added pre-commit settings

--- products/models.py
+++ products/models.py
@@ -1,51 +1,73 @@
-# -*- coding: utf-8 -*-
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-from sorl.thumbnail import ImageField
 from mptt.models import MPTTModel, TreeForeignKey
+from slugify import slugify
+from sorl.thumbnail import ImageField
 
 
 class Catalog(MPTTModel):
     name = models.CharField(max_length=50, unique=True)
-    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    parent = TreeForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
+    order = models.PositiveIntegerField(verbose_name=_("Order"))
+    slug = models.SlugField(editable=False, verbose_name=_("Slug"))
 
     def __str__(self):
         return self.name
 
+    # It is required to rebuild tree after save, when using order for mptt-tree
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        Catalog.objects.rebuild()
+
     class MPTTMeta:
-        order_insertion_by = ['name']
+        order_insertion_by = ["order"]
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Catalog, on_delete=models.SET_NULL, null=True, verbose_name=_('Category'))
-    image = ImageField(upload_to='product', verbose_name=_('Image'))
-    name = models.CharField(max_length=250, verbose_name=_('Product name'))
-    desc = models.TextField(verbose_name=_('Description'))
-    price = models.DecimalField(max_digits=6, decimal_places=0, default=0.01, verbose_name=_('Price'))
-    is_active = models.BooleanField(default=False, verbose_name=('Active'))
+    category = models.ForeignKey(
+        Catalog, on_delete=models.SET_NULL, null=True, verbose_name=_("Category")
+    )
+    image = ImageField(upload_to="product", verbose_name=_("Image"))
+    name = models.CharField(max_length=250, verbose_name=_("Product name"))
+    desc = models.TextField(verbose_name=_("Description"))
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, default=0.01, verbose_name=_("Price")
+    )
+    is_active = models.BooleanField(default=False, verbose_name=("Active"))
+    slug = models.SlugField(editable=False, verbose_name=_("Slug"))
 
     def __str__(self):
         return self.name
 
     @property
     def caption(self):
-        name = _('Product name')
-        name = f'{name}: {self.name}\n'
-        price = _('Price')
-        price = f'{price}: {self.price}\n'
-        desc = _('Description')
-        desc = f'{desc}: {self.desc}\n'
-        return f'{name}{desc}{price}'
+        name = _("Product name")
+        name = f"{name}: {self.name}\n"
+        price = _("Price")
+        price = f"{price}: {self.price}\n"
+        desc = _("Description")
+        desc = f"{desc}: {self.desc}\n"
+        return f"{name}{desc}{price}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 
 class Order(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('User'))
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_('Product'))
-    is_delivered = models.BooleanField(default=False, verbose_name=_('Delivered'))
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, verbose_name=_("User")
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, verbose_name=_("Product")
+    )
+    is_delivered = models.BooleanField(default=False, verbose_name=_("Delivered"))
     created = models.DateTimeField(auto_now_add=True, editable=False)
     edited = models.DateTimeField(auto_now=True, editable=False)
 
     def __str__(self):
-        return f'{self.user}: {self.product} - {self.created}'
+        return f"{self.user}: {self.product} - {self.created}"
